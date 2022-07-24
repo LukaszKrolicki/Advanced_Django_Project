40151616cfa08ca71f441a85535adce78470329f
SesneiBunny <lukasmatrix12345@gmail.com>
2022-07-24 23:24:51 +0200

    CRUD added and working

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns=[
     path('room/<str:pk>', views.room, name="room"),
 
     path('create_room/', views.createRoom, name="create_room"),
-    path('update_room/<str:pk>/', views.updateRoom, name="update_room")
+    path('update_room/<str:pk>/', views.updateRoom, name="update_room"),
+    path('delete_room/<str:pk>/', views.deleteRoom, name="delete_room")
 ]
